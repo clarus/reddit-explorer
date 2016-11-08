c250ee73041da80a69d572f00acc6c82db23f2a7
Guillaume Claret <dev@clarus.me>
2016-11-08 17:49:15 +0100

    Use redux-ship-logger instead of adhoc logger

--- src/index.js
+++ src/index.js
@@ -5,41 +5,15 @@ import ReactDOM from 'react-dom';
 import createHistory from 'history/createBrowserHistory'
 import './index.css';
 import * as Ship from 'redux-ship';
+import {logControl} from 'redux-ship-logger';
 import * as Effect from './effect';
 import Index from './index/view';
 import * as Controller from './index/controller';
 import * as Route from './route';
 import store from './store';
 
-function padTwoDigits(n: number): string {
-  return n < 10 ? '0' + String(n) : String(n);
-}
-
-function isoLocaleTimeString(date: Date): string {
-  return padTwoDigits(date.getHours()) + ':' +
-    padTwoDigits(date.getMinutes()) + ':' +
-    padTwoDigits(date.getSeconds()) + '.' +
-    (date.getMilliseconds() / 1000).toFixed(3).slice(2, 5);
-}
-
-function snapshotShape<Effect, Action>(
-  snapshot: Ship.Snapshot<Effect, Action>
-): string[] {
-  return snapshot.map((snapshotItem) => snapshotItem.type);
-}
-
-function* controller(action: Controller.Action) {
-  const {snapshot} = yield* Ship.snap(Controller.control(action));
-  const now = new Date();
-  console.group('ship', '@', isoLocaleTimeString(now), action.type);
-  console.log('action', action);
-  console.log('shape', ...snapshotShape(snapshot));
-  console.log('snapshot', snapshot);
-  console.groupEnd();
-}
-
 function dispatch(action: Controller.Action): void {
-  Ship.run(Effect.run, store, controller(action));
+  Ship.run(Effect.run, store, logControl(Controller.control)(action));
 }
 
 const history = createHistory();
